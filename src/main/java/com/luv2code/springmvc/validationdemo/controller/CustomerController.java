d5c1801798fb5518cdac56b20f74f54ca7753e50
Nadeem Ansari <nadeem_ansari64@outlook.com>
2025-02-27 20:13:07 -0600

    add init binder to customer controller add free passes and postal code to student

--- src/main/java/com/luv2code/springmvc/validationdemo/controller/CustomerController.java
+++ src/main/java/com/luv2code/springmvc/validationdemo/controller/CustomerController.java
@@ -2,15 +2,24 @@ package com.luv2code.springmvc.validationdemo.controller;
 
 import com.luv2code.springmvc.validationdemo.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model model) {
--- src/main/java/com/luv2code/springmvc/validationdemo/model/Customer.java
+++ src/main/java/com/luv2code/springmvc/validationdemo/model/Customer.java
@@ -1,7 +1,6 @@
 package com.luv2code.springmvc.validationdemo.model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -10,6 +9,15 @@ public class Customer {
     @NotNull(message = "Last Name is required.")
     @Size(min = 1, message = "Last Name must be at least 1 character long.")
     private String lastName;
+
+    @NotNull(message = "Free Passes is required.")
+    @Min(value = 0, message = "Free Passes must be between 0 and 10.")
+    @Max(value = 10, message = "Free Passes must be between 0 and 10.")
+    private Integer freePasses;
+
+    @NotNull(message = "Postal Code is required.")
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Postal Code must be 5 characters or digits.")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -25,5 +33,21 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
